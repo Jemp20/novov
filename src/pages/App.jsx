4dc7abb49dd50266e46b703661031262b254edd1
Jemp20 <jmp58642@gmail.com>
2026-04-17 15:04:20 -0500

    fix: chatbot solo en tienda, no en admin

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,12 +1,19 @@
 import { Routes, Route } from "react-router-dom";
 import AppStore from "../AppStore.jsx";
 import AppAdmin from "./AppAdmin.jsx";
+import NovovChatbot from "../components/NovovChatbot";
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/admin" element={<AppAdmin />} />
-      <Route path="/" element={<AppStore />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/admin" element={<AppAdmin />} />
+        <Route path="/" element={<AppStore />} />
+      </Routes>
+
+      <Routes>
+        <Route path="/" element={<NovovChatbot />} />
+      </Routes>
+    </>
   );
 }
